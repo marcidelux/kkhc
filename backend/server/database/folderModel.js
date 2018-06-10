5f877f93893f9c6b1d1b10dd71aa3f26fe6f8d22
Nyárai András <andrasnyarai@gmail.com>
2018-06-10 23:06:41 +0200

    mongo seed the db with folderstructure

--- backend/server/database/folderModel.js
+++ backend/server/database/folderModel.js
@@ -1,9 +1,14 @@
-
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('folders', { 
+let connection = mongoose.createConnection('mongodb://karma:coldcold@db:27017/kkhcfiles?authSource=admin');
+
+let folderSchema = new mongoose.Schema({ 
   name: String,
   path: String,
   contains: Array,
   hash: Number
-}, 'folders')
+});
+
+let Folder = connection.model('folders', folderSchema, 'folders')
+
+module.exports = {Folder, connection};
