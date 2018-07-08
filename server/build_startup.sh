640d71ebb3c00caab25a2233a8cdef6610b9dbed
donbattery <mikloslorinczi.it@gmail.com>
2018-07-08 19:55:52 +0200

    test03

--- server/build_startup.sh
+++ server/build_startup.sh
@@ -2,11 +2,14 @@
 
 until nc -z db 27017
 do
+    echo "waiting for Database ..."
     sleep 1
 done
 
 cd server
 
+echo "Seeding database with folder structure..."
 node ./database/dbSeed.js
 
+echo "Starting Server..."
 node app.js
